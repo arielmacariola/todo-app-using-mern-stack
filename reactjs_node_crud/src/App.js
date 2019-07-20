36944c262a3ad671cbe0a354d7388a885db8cf8e
Ariel Macariola <arielmacariola.profs@yahoo.com>
2019-07-20 18:14:36 +0800

    add front-end files

---
+++ reactjs_node_crud/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.png";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a href="http://github.com/arielmacariola" target="_blank" rel="noopener noreferrer" className="navbar-brand">
+              <img src={logo} width="70" alt="Ariel Mac Todo App" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
