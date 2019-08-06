d79093b3d055980396687844a7f0ab3e507f1b74
Ariel Macariola <arielmacariola.profs@yahoo.com>
2019-08-06 19:49:45 +0800

    Display todos list from mongoDB collection

--- reactjs_node_crud/src/App.js
+++ reactjs_node_crud/src/App.js
@@ -18,7 +18,11 @@ class App extends Component {
               <img src={logo} width="70" alt="Ariel Mac Todo App" />
             </a>
             <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-            <div className="collapse navbar-collapse">
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav">
                 <li className="nav-item">
                   <Link to="/" className="nav-link">Todos</Link>
